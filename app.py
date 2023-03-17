d949cc3c19b26926727ee5bc86ff36143493d437
cmsulzbeck <cmsulzbeck@hotmail.com>
2023-03-17 18:20:52 -0300

    reestruturações de venv e alterações de banco de dados

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from sqlalchemy.sql import text
 from sqlalchemy.exc import IntegrityError
 from datetime import datetime, timedelta, timezone
 
 import jwt
 import bcrypt
-import io, os, sys, config
+import config
+import json
 
 # TODO modularizar a aplicação
 
 app = Flask(__name__)
+ma = Marshmallow(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth_api.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(60), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    birth_date = db.Column(db.DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    date_created = db.Column(db.DateTime, server_default=text("CURRENT_TIMESTAMP"))
     session_token = db.Column(db.String(255), unique=True, default=None)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.session_token}')"
+    
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'email': self.email,
+            'birth_date': self.birth_date,
+            'session_token': self.session_token,
+        }
+    
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        #fields to expose
+        fields = ("email", "username", "session_token", "date_created")
+        model = User
     
 cfg = config.ConfigError('config.cfg')
 
@@ -40,7 +63,7 @@ def login():
     # print(f"Hashed Password: {hashed_password}")
 
     if not user or not bcrypt.checkpw(password.encode('utf-8'), user.password):
-        return jsonify({'message': "Invalid credentials"}), 401
+        return jsonify({'status': 'Failure', 'message': "Invalid credentials"}), 401
     
     message = {
         'iat': datetime.now(timezone.utc),
@@ -54,7 +77,7 @@ def login():
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({'token': token})
+    return jsonify({'status': 'Success', 'token': token})
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -62,25 +85,26 @@ def register():
 
     username = data.get('username')
     email = data.get('email')
+    birth_date = data.get('birth_date')
     password = data.get('password')
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    if not all([username, email, password]):
-        return jsonify({'message': 'Missing data'}), 400
+    if not all([username, email, birth_date, password]):
+        return jsonify({'status': 'Failure', 'message': 'Missing data'}), 400
     
-    new_user = User(username=username, email=email, password=hashed_password)
+    new_user = User(username=username, email=email, birth_date=birth_date, password=hashed_password)
 
     try:
         db.session.add(new_user)
         db.session.commit()
         print(f"Saving user {username} and password {hashed_password}")
-        return jsonify({'message': 'User created successfully'}), 201
+        return jsonify({'status': 'Success', 'message': 'User created successfully'}), 201
     except IntegrityError:
         db.session.rollback()
-        return jsonify({'message': 'Username already exists'}), 409
+        return jsonify({'status': 'Failure', 'message': 'Username already exists'}), 409
     except Exception as e:
         db.session.rollback()
-        return jsonify({'message': 'Unable to create user'}), 500
+        return jsonify({'status': 'Failure', 'message': 'Unable to create user'}), 500
 
 @app.route('/logoff', methods=['POST'])
 def logoff():
@@ -99,6 +123,11 @@ def logoff():
 def getUsers():
     users = User.query.filter_by().all()
     print(f"Users found: f{users}")
+    users_dict = []
+    for user in users:
+        users_dict.append(user.to_dict())
+    
+    print("users_dict: ", users_dict)
 
     return jsonify({'message': 'Retrieved Users information!'})
     
